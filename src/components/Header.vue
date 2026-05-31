06996f6478bbf49d34b0dd7a6b4f2a8975ce7bc2
Tnozone <maxnocquet@gmail.com>
2026-05-31 21:08:00 +0200

    basic header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,77 @@
 <template>
   <header>
-    <h1 class="text-primary, text-center">Recipedeck</h1>
+    <h1 class="text-center bg-header py-3 mb-0 bg-header text-white fs-1">Recipedeck</h1>
 
-    <nav class="navbar">
-      <router-link to="/" class="nav-item">Home</router-link>
-      <router-link to="/upload" class="nav-item">Upload Recipe</router-link>
-      <router-link to="/recipes" class="nav-item">My Recipes</router-link>
-      <router-link to="/fav" class="nav-item">Favorite Recipes</router-link>
-      <router-link to="/login" class="nav-item">Log In</router-link>
-      <router-link to="/signup" class="nav-item">Sign Up</router-link>
+    <nav class="navbar navbar-expand-lg bg-header px-3 ms-auto">
+      <div class="container-fluid">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#mainNavbar"
+          aria-controls="mainNavbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <!-- Collapsible menu -->
+        <div
+          class="collapse navbar-collapse"
+          id="mainNavbar"
+        >
+          <div class="navbar-nav ms-auto">
+            <router-link to="/" class="nav-link text-white fs-4">
+              Home
+            </router-link>
+
+            <router-link to="/upload" class="nav-link text-white fs-4">
+              Upload Recipe
+            </router-link>
+
+            <router-link to="/recipes" class="nav-link text-white fs-4">
+              My Recipes
+            </router-link>
+
+            <router-link to="/fav" class="nav-link text-white fs-4">
+              Favorite Recipes
+            </router-link>
+
+            <router-link to="/login" class="nav-link text-white fs-4">
+              Log In
+            </router-link>
+
+            <router-link to="/signup" class="nav-link text-white fs-4">
+              Sign Up
+            </router-link>
+          </div>
+        </div>
+      </div>
     </nav>
   </header>
 </template>
+
+<style scoped>
+.bg-header {
+  background-color: #87ceeb;
+}
+
+.nav-link {
+  font-weight: 500;
+}
+
+.nav-link:hover {
+  text-decoration: underline;
+}
+
+.navbar-toggler {
+  border-color: rgb(255, 255, 255);
+}
+
+.navbar-toggler-icon {
+  width: 2.5rem;
+  height: 2.5rem;
+  background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 30 30'%3e%3cpath stroke='rgba%28255,255,255,1%29' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e");
+}
+</style>
