fc4e114ddd8c4f520944e0546974cd4bdc8abe1a
Tnozone <maxnocquet@gmail.com>
2026-06-02 20:28:25 +0200

    better card load

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,14 +2,23 @@
   <div>
     <p class="text-center">Welcome. Upload a recipe or browse for one you like.</p>
   </div>
-  <div class="row">
-    <div class="col-sm-6">
-      {{ Recipecard }}
+  <div class="container">
+    <div class="row">
+      <div
+        v-for="recipe in recipes"
+        :key="recipe.id"
+        class="col-md-4 mb-4"
+      >
+        <RecipeCard :recipe="recipe" />
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-  const currentUser =
-    JSON.parse(localStorage.getItem('currentUser'))
+import recipes from '../data/recipes.json'
+import RecipeCard from '../components/RecipeCard.vue'
+
+const currentUser =
+  JSON.parse(localStorage.getItem('currentUser'))
 </script>
