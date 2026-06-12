c194fe49fe1138276d199d68028427d7e52d0201
Tnozone <maxnocquet@gmail.com>
2026-06-12 14:20:41 +0200

    hashed passwords

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 import User from "../models/User.js";
 import Recipe from '../models/Recipe.js'
+import bcrypt from 'bcrypt'
 
 
 router.post("/register", async (req, res) => {
+  try {
     const existingUser = await User.findOne({
         email: req.body.email
     })
@@ -16,11 +18,27 @@ router.post("/register", async (req, res) => {
         })
     }
 
-    const user = new User(req.body);
+    const hashedPassword = await bcrypt.hash(
+      req.body.password,
+      10
+    )
+
+    const user = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword
+    })
 
     await user.save();
 
     res.json(user);
+  }
+
+  catch (err) {
+    res.status(500).json({
+      message: err.message
+    })
+  }
 });
 
 router.post('/login', async (req, res) => {
@@ -39,7 +57,12 @@ router.post('/login', async (req, res) => {
     })
   }
 
-  if (user.password !== password) {
+  const passwordMatch = await bcrypt.compare(
+    password,
+    user.password
+  )
+
+  if (!passwordMatch) {
     return res.status(401).json({
       message: 'Invalid username/email or password'
     })
